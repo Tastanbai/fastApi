01609a2febc4142beddb7b4380760253c51a15b1
Tastan <tastanbay02@gmail.com>
2025-01-30 14:37:09 +0500

    clear commit

--- face_save.py
+++ face_save.py
@@ -1,74 +1,7 @@
-# import cv2
-# import dlib
-# import os
-# from datetime import datetime
-
-
-# def extract_and_save_faces(image_path, output_folder, face_margin=20):
-#     """
-#     Извлекает лица из изображения и сохраняет их в указанной папке.
-    
-#     :param image_path: Путь к исходному изображению.
-#     :param output_folder: Папка для сохранения изображений лиц.
-#     :param face_margin: Дополнительный отступ вокруг лица (в пикселях).
-#     """
-#     # Убедиться, что выходная папка существует
-#     if not os.path.exists(output_folder):
-#         os.makedirs(output_folder)
-
-#     # Загрузка изображения
-#     image = cv2.imread(image_path)
-#     if image is None:
-#         raise ValueError("Не удалось загрузить изображение. Проверьте путь к файлу.")
-
-#     # Инициализация детектора лиц dlib
-#     detector = dlib.get_frontal_face_detector()
-
-#     # Конвертация изображения в оттенки серого (требуется для dlib)
-#     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-#     # Обнаружение лиц
-#     faces = detector(gray_image)
-
-#     if len(faces) == 0:
-#         print("Лица не обнаружены на изображении.")
-#         return
-
-#     # Итерация по всем обнаруженным лицам
-#     for i, face in enumerate(faces):
-#         x1 = max(face.left() - face_margin, 0)
-#         y1 = max(face.top() - face_margin, 0)
-#         x2 = min(face.right() + face_margin, image.shape[1])
-#         y2 = min(face.bottom() + face_margin, image.shape[0])
-
-#         # Вырезать лицо из изображения
-#         face_image = image[y1:y2, x1:x2]
-
-#         # Уменьшить размер лица до ширины 300 пикселей, если оно больше
-#         face_height, face_width = face_image.shape[:2]
-#         if face_width > 300:
-#             scale_ratio = 300 / face_width
-#             new_width = 300
-#             new_height = int(face_height * scale_ratio)
-#             face_image = cv2.resize(face_image, (new_width, new_height), interpolation=cv2.INTER_AREA)
-
-#         # Генерация уникального имени файла по локальной дате и времени
-#         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
-#         output_path = os.path.join(output_folder, f"face_{timestamp}.jpg")
-
-#         # Сохранение лица в выходную папку
-#         cv2.imwrite(output_path, face_image)
-#         print(f"Лицо сохранено: {output_path}")
-
-
-# # Пример использования
-# extract_and_save_faces("files/facetau.jpg", "faces_folder", face_margin=20)
-
 import cv2
 import dlib
 import os
 from datetime import datetime
-
 
 def extract_and_save_faces(image_path, output_folder, face_margin=20):
     print(f"Извлечение лица из файла: {image_path}")
--- save_emb.py
+++ save_emb.py
@@ -1,35 +1,3 @@
-# import face_recognition
-# from io import BytesIO
-# import pickle
-# import requests
-
-# # Путь к изображению и файлу для хранения эмбеддингов
-# url = "faces_folder/face2.jpg"
-
-# image_of_me = face_recognition.load_image_file(url)
-# my_face_encoding = face_recognition.face_encodings(image_of_me)[0]
-# with open(f"emb/emb-new.pkl", "wb") as file:
-#     pickle.dump(my_face_encoding, file)
-# print(f"Эмбеддинг сохранен в файл")
-
-
-# import face_recognition
-# import pickle
-
-# def save_embedding_to_file(image_path, output_path):
-#     print(f"Создание эмбеддинга для файла: {image_path}")
-#     image = face_recognition.load_image_file(image_path)
-#     face_encodings = face_recognition.face_encodings(image)
-
-#     if not face_encodings:
-#         raise ValueError(f"Лицо не обнаружено в изображении: {image_path}")
-
-#     face_encoding = face_encodings[0]
-#     with open(output_path, "wb") as file:
-#         pickle.dump(face_encoding, file)
-#     print(f"Эмбеддинг сохранён: {output_path}")
-
-
 import face_recognition
 import pickle
 
